276886ddeeeaf3f0747c2017168a708f7d5aac3d
jack <whisler1@gmail.com>
2021-12-13 18:06:29 -0600

    adds frequent letter solution

---
+++ fizzbuzz.js
@@ -0,0 +1,26 @@
+// Write a function that prints out every number from 1 to N, with the following exceptions:
+
+// If the number is divisible by 3, print out "FIZZ".
+// If the number is divisible by 5, print out "BUZZ".
+// If the number is divisible by both 3 and 5, print out "FIZZBUZZ".
+
+// Gameplan:
+// divisible by 3 & 5 is most specific, should be first if
+// then else if 3 , 5
+
+const fizzbuzz = (n) => {
+  let i = 1;
+  while (i <= n) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FIZZBUZZ");
+    } else if (i % 3 === 0) {
+      console.log("FIZZ");
+    } else if (i % 5 === 0) {
+      console.log("BUZZ");
+    } else {
+      console.log(i);
+    }
+    i++;
+  }
+};
+fizzbuzz(50);
---
+++ frequent_letter.rb
@@ -0,0 +1,21 @@
+# Given a string, find the most commonly occurring letter.
+
+# Input: “peter piper picked a peck of pickled peppers”
+# Output: “p
+# Split string into individ characters array
+# Loop through array, add letter to hash and initial count to 1, if in hash add to count
+
+def frequent_letter(str)
+  letters = {}
+  letters_arr = str.split("") 
+  p letters_arr
+  letters_arr.each do |letter|
+    if letters[letter]
+      letters[letter] += 1
+    else 
+      letters[letter] = 1
+    end 
+  end
+  return letters.max_by { |k, v| v }[0]
+end
+p frequent_letter("peter piper picked peck")
