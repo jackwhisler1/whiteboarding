6aafd225d50d09fb2c53a655de944b2df457c4d3
jack <whisler1@gmail.com>
2022-01-20 11:32:35 -0600

    tests hw

--- homework_testing.rb
+++ homework_testing.rb
@@ -1,33 +1,8 @@
-  # def find_longest_word(string)
+def split_array(array, n)
+  new_array = []
+  array.each_slice(n) { |slice| new_array << slice }
+  new_array
+end
 
-  #   array = string.split
-
-  #   word_length = []
-  #   array.map do |x|
-  #     word_length << x.length
-  #   end
-    
-  #   return array[word_length.index(word_length.max)]
-      
-  # end
-
-  # p find_longest_word("What if I had a asefadki stupidly  long word?")
-
-  # Write a method called find_longest_word, which will accept a string as a parameter (usually a sentence), and return another string that will be the longest word in that sentence.
-  # function take a string
-  # string convert into an array words
-  # loop throughh and check length of each word
-  # keep track of the longest
-  # return the longest
-
-  def longest_word(str)
-    words = str.split(" ") 
-    longest_word = ""
-    words.each do |word|
-      if word.length > longest_word.length
-        longest_word = word
-      end
-    end
-    longest_word 
-  end
-  p longest_word("What if I had a asefadki stupidly  long word?")
+p split_array([0, 1, 2, 3, 4, 5], 2) 
+p split_array([0, 1, 2, 3, 4, 5], 3)
