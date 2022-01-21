cad79313d2cdeb1705b0b7f9f198883baf0092ac
jack <whisler1@gmail.com>
2022-01-21 11:57:35 -0600

    take home init

--- homework_testing.rb
+++ homework_testing.rb
@@ -1,8 +1,5 @@
-def split_array(array, n)
-  new_array = []
-  array.each_slice(n) { |slice| new_array << slice }
-  new_array
+def split_array(array, number)
+  array.each_slice(number).to_a
 end
 
-p split_array([0, 1, 2, 3, 4, 5], 2) 
-p split_array([0, 1, 2, 3, 4, 5], 3)
+p split_array([0, 1, 2, 3, 4, 5], 7) 
