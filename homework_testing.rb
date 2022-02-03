c9d429aa4a78f2a3640c8b9c105a5842bc521037
jack <whisler1@gmail.com>
2022-02-03 12:17:28 -0600

    updates hw test

--- homework_testing.rb
+++ homework_testing.rb
@@ -1,5 +1,21 @@
-def split_array(array, number)
-  array.each_slice(number).to_a
+def find_missing_letter(string)
+  arrayed_letters = [*string[0]..string[string.length-1]]
+  hashed_letters = {}
+  string.chars.each do |letter|
+    hashed_letters[letter] = true
+  end
+  missing_letters = []
+  arrayed_letters.each do |letter|
+    if !hashed_letters[letter]
+      missing_letters << letter
+    end
+  end
+  if missing_letters == []
+    return nil
+  end
+  return missing_letters.join("")
 end
 
-p split_array([0, 1, 2, 3, 4, 5], 7) 
+p find_missing_letter("opqrsuv")  #=> "t"
+p find_missing_letter("xyz")      #=> nil
+p find_missing_letter("hiklno")  #=> "jm"
